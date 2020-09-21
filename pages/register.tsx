2ec9ea101fedbcadcd8235155f5d439526bc7946
Sidinei Silva <sidinei.silva02@gmail.com>
2020-09-21 15:47:59 -0300

    Incluindo tela de registro

---
+++ pages/register.tsx
@@ -0,0 +1,60 @@
+import React, { FormEvent } from 'react';
+
+// import { Container } from './styles';
+
+const Register: React.FC = () => {
+  const [email, setEmail] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    alert(`name: ${name}`);
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1>Register</h1>
+      <form
+        onSubmit={onSubmit}
+        style={{ border: '1px solid #ccc', padding: '25px 50px' }}
+      >
+        <h5>Name:</h5>
+        <input
+          name="name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+
+        <h5>Email:</h5>
+        <input
+          name="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <h5>Senha:</h5>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+
+        <h5 style={{ textAlign: 'center' }}>
+          <button type="submit">Entrar</button>
+        </h5>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
