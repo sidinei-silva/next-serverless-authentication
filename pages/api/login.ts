7f53f6f438d8a1dd0dcc46b8274183280e9a61a6
Sidinei Silva <sidinei.silva02@gmail.com>
2020-09-21 17:02:58 -0300

    Criando rota api login

---
+++ pages/api/login.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  const { email, password } = req.body;
+
+  if (req.method !== 'POST') {
+    res.statusCode = 404;
+    res.json({ status: 'error', message: 'Cannot GET /api/login' });
+    return;
+  }
+
+  if (!req.body) {
+    res.statusCode = 400;
+    res.json({ status: 'error', message: 'Body not present' });
+    return;
+  }
+
+  if (!email) {
+    res.statusCode = 400;
+    res.json({ status: 'error', message: 'Email not present' });
+    return;
+  }
+
+  if (!password) {
+    res.statusCode = 400;
+    res.json({ status: 'error', message: 'Password not present' });
+    return;
+  }
+
+  res.statusCode = 200;
+  res.json({ email, password });
+};
