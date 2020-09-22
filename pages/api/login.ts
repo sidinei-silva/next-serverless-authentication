b9297a244c8d17787064310e7fab86b148cc212a
Sidinei Silva <sidinei.silva02@gmail.com>
2020-09-22 12:16:04 -0300

    Criando função de conexão db em login api

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,4 +1,27 @@
+import { MongoClient, Db } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
+import url from 'url';
+
+let cachedDb: Db = null;
+
+async function connectToDatabase(uri: string) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const dbName = url.parse(uri).pathname.substr(1);
+
+  const db = client.db(dbName);
+
+  cachedDb = db;
+
+  return db;
+}
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   const { email, password } = req.body;
